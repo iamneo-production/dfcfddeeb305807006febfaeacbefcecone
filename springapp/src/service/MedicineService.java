7a41aac60f85668c52b9c195369ae3d9b5324a8f
Iamneo <techadmin@iamneo.ai>
2023-10-06 04:34:18 +0000

    Backup Commit

--- springapp/src/service/MedicineService.java
+++
@@ -1,9 +0,0 @@
-package com.pharmacy.service;
-
-import com.pharmacy.model.Medicine;
-
-public interface MedicineService {
-    boolean addMedicine(Medicine medicine);
-
-        Medicine updateMedicine(int medicineId, Medicine updatedMedicine);
-        }
